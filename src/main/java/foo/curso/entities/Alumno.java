c5682893403f3fcbb5058986cd638b6d412cb669
genuinefafa <genuinefafa@gmail.com>
2013-09-30 15:11:07 -0300

    Ya no depende de la db la clase Alumno

--- src/main/java/foo/curso/entities/Alumno.java
+++ src/main/java/foo/curso/entities/Alumno.java
@@ -1,12 +1,5 @@
 package foo.curso.entities;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Alumno {
